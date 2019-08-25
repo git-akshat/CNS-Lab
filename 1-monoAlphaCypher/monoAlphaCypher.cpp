2b0cdc0e5924388bcd2aa66d5818b1c09d26e200
Akshat <mr.akshatagarwal26@gmail.com>
2019-08-25 14:29:03 +0530

    added mono alphabetic cypher

--- 1-monoAlphaCypher/monoAlphaCypher.cpp
+++ 1-monoAlphaCypher/monoAlphaCypher.cpp
@@ -1,14 +1,14 @@
 /*********************************************************************************
 Perform encryption and decryption using mono-alphabetic cipher. 
 The program should support the following:
-i.	Construct an input file named plaintext.txt (consisting of 1000 alphabets,
-	without any space or special characters) 
-ii.	Compute key space (Permutation of set of all letters appeared in plaintext.txt:
-	there are n! permutations of a set of n elements)
+i.  Construct an input file named plaintext.txt (consisting of 1000 alphabets,
+    without any space or special characters) 
+ii. Compute key space (Permutation of set of all letters appeared in plaintext.txt:
+    there are n! permutations of a set of n elements)
 iii.Encrypt the characters of plaintext.txt using any one key from (ii)
-	and store the corresponding ciphertext characters in ciphertext.txt
-iv.	Compute the frequency of occurrence of each alphabet in both plaintext.txt
-	and ciphertext.txt and tabulate the results as follows	
+    and store the corresponding ciphertext characters in ciphertext.txt
+iv. Compute the frequency of occurrence of each alphabet in both plaintext.txt
+    and ciphertext.txt and tabulate the results as follows	
 ***********************************************************************************/
 
 #include<bits/stdc++.h>
@@ -110,7 +110,7 @@ void showFrequency(string pt , string ct){
 	cout<<"\nFrequency\t\tPlaintext Character\t\tCiphertext character" <<endl;
 	cout<<"=========\t\t===================\t\t====================" <<endl;
 	for(int i =0 ;i < pt.length() ; i++){
-		cout<< (float)mPlain[pt[i]]/pt.length() << "\t\t\t" << pt[i] << "\t\t\t" << ct[i] << endl ;
+		cout<< (float)mPlain[pt[i]]/pt.length() << "\t\t\t\t" << pt[i] << "\t\t\t\t" << ct[i] << endl ;
 	}
 }
 
@@ -153,3 +153,43 @@ int main()
 	writeCypherText(cyphertext) ;
 	showFrequency(plaintext , cyphertext) ;
 }
+
+
+/****************************** OUTPUT-1 ****************************************
+Plain text =    HelloWorld
+Unique chars =  HWdelor
+Chosen key =    WHoedrl
+
+Frequency               Plaintext Character             Ciphertext character
+=========               ===================             ====================
+0.1                             H                               W
+0.1                             e                               e
+0.3                             l                               d
+0.3                             l                               d
+0.2                             o                               r
+0.1                             W                               H
+0.2                             o                               r
+0.1                             r                               l
+0.3                             l                               d
+0.1                             d                               o
+
+/****************************** OUTPUT-2 ****************************************
+Plain text =    workisworship
+Unique chars =  hikoprsw
+Chosen key =    rphskowi
+
+Frequency               Plaintext Character             Ciphertext character
+=========               ===================             ====================
+0.153846                                w                               i
+0.153846                                o                               s
+0.153846                                r                               o
+0.0769231                               k                               h
+0.153846                                i                               p
+0.153846                                s                               w
+0.153846                                w                               i
+0.153846                                o                               s
+0.153846                                r                               o
+0.153846                                s                               w
+0.0769231                               h                               r
+0.153846                                i                               p
+***********************************************************************************/
