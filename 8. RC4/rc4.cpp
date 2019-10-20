09787ff5d0adbe4173f3e60c8f8af177a7888b22
akshat <mr.akshatagarwal26@gmail.com>
2019-10-20 09:04:24 +0530

    reduced code lines

--- 8. RC4/rc4.cpp
+++ 8. RC4/rc4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int S[256], T[256], k[256], keyStream[256];
+    int S[256], T[256], keyStream[256];
     string ptString, keyString, dtString = "";
     int pt[256], ct[256], dt[256];
 
@@ -18,16 +18,11 @@ int main()
         cout << pt[i] << " ";
     }
 
-    for(int i=0; i<keyString.length(); i++)
-    {
-        k[i] = keyString[i];
-    }
-
     // Initialization
     for(int i=0; i<256; i++)
     {
         S[i] = i;
-        T[i] = k[i % keyString.length()];
+        T[i] = (int)keyString[i%keyString.length()];
     }
 
     // Initial permutation
@@ -39,6 +34,7 @@ int main()
     }
 
     // Stream Generation
+    cout << "\nKey Stream \t: ";
     j=0;
     for(int i=0; i<n; i++) // generate keystream of same length as plaintext
     {
@@ -46,6 +42,7 @@ int main()
         swap(S[i], S[j]);
         int t = (S[i] + S[j]) % 256;
         keyStream[i] = S[t];
+        cout << keyStream[i] << " ";
     }
 
     // Encryption
@@ -66,3 +63,25 @@ int main()
     }
     cout << "\nDecrypted text \t: " << dtString << endl;
 }
+
+/******************* output-1 ************************
+Enter message : Siddaganga
+Enter key     : Institute
+
+Plain text      : 83 105 100 100 97 103 97 110 103 97
+Key Stream      : 236 34 53 234 27 158 64 219 222 102
+Cipher Text     : 191 75 81 142 122 249 33 181 185 7
+Decrypted text  : 83 105 100 100 97 103 97 110 103 97
+Decrypted text  : Siddaganga
+******************************************************/
+
+/******************* output-2 ************************
+Enter message : Washington
+Enter key     : DC
+
+Plain text      : 87 97 115 104 105 110 103 116 111 110
+Key Stream      : 116 117 178 145 231 124 255 200 240 115
+Cipher Text     : 35 20 193 249 142 18 152 188 159 29
+Decrypted text  : 87 97 115 104 105 110 103 116 111 110
+Decrypted text  : Washington
+******************************************************/
