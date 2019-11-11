56aae61acd7f0623c15732d4608c3dc897ea75a5
akshat <mr.akshatagarwal26@gmail.com>
2019-11-12 02:16:06 +0530

    added 13. Diffie-Hellman

---
+++ _13. Diffie-Hellman/client.cpp
@@ -0,0 +1,61 @@
+# include <bits/stdc++.h>
+# include <arpa/inet.h> 
+using namespace std;
+
+int connectToServer(const char* ip, int port)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {AF_INET, htons(port),inet_addr(ip)};
+    
+    if(connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0 ){
+        cout << "\nRun server program first." << endl; exit(0);
+    }else{
+        cout << "\nClient is connected to Server." << endl; 
+    }
+    return sock;
+}
+
+long powermod(long a, long b, long  q)
+{
+	long res=1;
+	for(long i=0;i<b;i++)
+	{
+		res=(res*a)%q;
+	}
+	return res;
+}
+
+int main()
+{
+    char ip[50];
+    int port;
+    cout << "\nEnter server's IP address: "; cin >> ip;
+    cout << "Enter port : "; cin >> port;
+    int sock = connectToServer(ip, port);
+    
+	srand(time(NULL));
+	long q, alpha;
+	cout<<"\nEnter a prime number, q : "; cin >> q;
+	cout<<"Enter primitive root of q, alpha : "; cin >> alpha;
+	
+	long Xc = rand() % (q-1) +1; // client's private key (1<Xa<q)
+	cout<< "\nClient's private key, Xc = " << Xc <<endl;
+	
+	long Yc = powermod(alpha, Xc, q); // client's public key
+	send(sock, &Yc, sizeof(Yc), 0);	 // send client's public key
+	cout<< "Client's public key,  Yc = " << Yc <<endl;
+	
+	long Ys;
+	recv(sock, &Ys, sizeof(Ys), 0);	 // recv server's public key
+	cout<< "\nServer's public key,  Ys = " << Ys <<endl;
+	
+	long k = powermod(Ys,Xc,q);	
+	cout<<"\nSecret Key, k = "<<k<<endl;
+
+	int cipher;	
+	recv(sock, &cipher, sizeof(cipher), 0);
+	cout<<"\nMessage received from Server  : " << cipher << endl;
+	
+	int decipher = cipher ^ k;	
+	cout << "Decrpyted message : " << decipher << endl << endl;
+}
---
+++ _13. Diffie-Hellman/server.cpp
@@ -0,0 +1,61 @@
+# include <bits/stdc++.h>
+# include <arpa/inet.h> 
+using namespace std;
+
+int createServer(int port)
+{
+	int sersock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {AF_INET, htons(port), INADDR_ANY};
+    
+    bind(sersock, (struct sockaddr *) &addr, sizeof(addr));
+    cout << "\nServer Online. Waiting for client...." << endl;
+    
+    listen(sersock, 5);
+    int sock = accept(sersock, NULL, NULL);
+    cout << "Connection Established." << endl;
+    return sock;
+}
+
+long powermod(long a, long b, long  q)
+{
+	long res=1;
+	for(long i=0;i<b;i++)
+	{
+		res=(res*a)%q;
+	}
+	return res;
+}
+
+int main()
+{
+    int port;
+    cout << "\nEnter port : "; cin >> port;
+    int sock = createServer(port);
+    
+	
+	long q, alpha;
+	cout<<"\nEnter a prime number, q : "; cin >> q;
+	cout<<"Enter primitive root of q, alpha : "; cin >> alpha;
+	
+	long Yc;
+	recv(sock, &Yc, sizeof(Yc), 0);	 // recv client's public key
+	cout<< "\nClient's public key,  Yc = " << Yc <<endl;
+	
+	srand(time(NULL));
+	long Xs = rand() % (q-1) +1; // server's private key (1<Xa<q)
+	cout<< "\nServer's private key, Xs = " << Xs <<endl;
+	
+	long Ys = powermod(alpha, Xs, q); // server's public key
+	send(sock, &Ys, sizeof(Ys), 0);	 // send server's public key
+	cout<< "Server's public key,  Ys = " << Ys <<endl;
+	
+	long k = powermod(Yc,Xs,q);	
+	cout<<"\nSecret Key, k = "<<k<<endl;
+
+	int msg;
+	cout<<"\nEnter a message(integer) to send : "; cin>>msg;
+	
+	int cipher=msg^k;
+	send(sock, &cipher, sizeof(cipher), 0);
+	cout << "Encrypted msg sent to client: " << cipher << endl << endl;
+}
