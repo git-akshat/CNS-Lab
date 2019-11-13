bf23f2fd2f1bb092521d327a8bca19c9bfdcbec2
Akshat Agarwal <mr.akshatagarwal26@gmail.com>
2019-11-14 00:42:13 +0530

    added working code for 12th

--- _12. RSA (Authentication)/client.cpp
+++ _12. RSA (Authentication)/client.cpp
@@ -2,12 +2,12 @@
 # include <arpa/inet.h> 
 using namespace std;
 
-int p, q, n, phi; // global variables
+int p, q, e, d, n, phi; // global variables
 int PUc[2], PRc[2];
 int PUs[2];
 int sock;
 
-int connectToServer(const char* ip, int port)
+void connectToServer(const char* ip, int port)
 {
     sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {AF_INET, htons(port), inet_addr(ip)};
@@ -26,11 +26,10 @@ int gcd(int a, int b)
 
 void genKey()
 {
-    cout << "\nEnter two prime numbers : "; cin >> p >> q;
-    int n = p * q ;
-    int phi = (p-1) * (q-1);
+    cout << "\nEnter two prime numbers (>100) : "; cin >> p >> q;
+    n = p * q ;
+    phi = (p-1) * (q-1);
 
-    int e, d;
     srand(time(NULL));
     do{ e = rand()%(phi-2)+2; } while(gcd(e,phi) != 1);
     for(d=1; d<phi; d++)
@@ -38,8 +37,8 @@ void genKey()
         if((d*e)%phi == 1) break;
     }
     
-    PUc[2] = {e, n}; // public key
-    PRc[2] = {d, n}; // private key
+    PUc[0] = e; PUc[1] = n; // public key
+    PRc[0] = d; PRc[1] = n; // private key
     cout << "\nPublic key , PUc = {" << e << ", " << n << "}" << endl;
     cout <<   "Private key, PRc = {" << d << ", " << n << "}" << endl;
 }
@@ -48,13 +47,12 @@ void shareKey()
 {
     recv(sock, &PUs, sizeof(PUs), 0); // receive public key from server
     send(sock, &PUc, sizeof(PUc), 0); // send client's public key to server
-
-    cout << "\nPublic key received from server : {" << PUs[0] << ", " << PUs[1] << "}" << endl;
+    cout << "Public key received from server, PUs = {" << PUs[0] << ", " << PUs[1] << "}" << endl;
     cout << "\nSent client's Public key to server." << endl;
 }
 
 // M = C^d mod n
-int encryptDecrypt(int M, int P[2])
+int encrypt(int M, int P[2])
 {
     int C=1;
     for(int i=1; i<=P[0]; i++)
@@ -82,32 +80,62 @@ int main()
     // step-1: recv cipher from server and Dec(PRc, [N1||ID])
     int cipher;
     recv(sock, &cipher, sizeof(cipher), 0);
-    cout << "Received encrypted (N1||ID) from server : " << cipher;
+    cout << "\nReceived encrypted (N1||ID) from server : " << cipher << endl;
     int msg = decrypt(cipher, PRc);
     int N1 = msg/100;
     int ID = msg%100;
-    cout << "\nServer's nonce, N1 = " << N1;
-    cout << "\nServer's ID = " << ID;
+    cout << "Decrypted Server's ID,   IDs = " << ID << endl;
+    cout << "Decrypted Server's nonce, N1 = " << N1 << endl;
 
     // step-2: send En(PUs, (N1||N2)) to server
     int N2 = rand() % 100; // nonce
+    cout << "\nNonce generated, N2 = " << N2 << endl;
     msg = N1*100 + N2;
     cipher = encrypt(msg, PUs);
     send(sock, &cipher, sizeof(cipher), 0);
-    cout << "Sent encrypted (N1||N2) to server.";
+    cout << "Sent encrypted (N1||N2) to server : " << cipher << endl;
 
     // step-3: recv enc(N2) from client and Dec(PRc, N2)
     recv(sock, &cipher, sizeof(cipher), 0);
+    cout << "\nReceived encrypted (N2) from server : " << cipher << endl;
     int N2s = decrypt(cipher, PRc);
-    cout << "\nDecrypted Client's Nonce, N2 = " << N2s;
-    if(N2s != N2) {cout << "\nNonce didn't match!"; exit(-1);}
+    cout << "Decrypted Client's Nonce, N2 = " << N2s << endl;
+    if(N2s != N2) {cout << "\nNonce didn't match!\n"; exit(-1);}
+    else {cout << "-----Authenticated Server-----" << endl;}
 
-    // step-4: recv cipher and perform k = Dec(PUs, Dec(PRc, M))
+    // step-4: recv cipher and perform k = Dec(PUs, Dec(PRc, C))
     int k;
     recv(sock, &cipher, sizeof(cipher), 0);
+    cout << "\nReceived cipher from Server : " << cipher << endl;
     k = decrypt(decrypt(cipher, PRc), PUs);
-    cout << "Decrypted Secret Key : " << k;
-
-    int M = decrypt(C, PR); // decrypted text
-    cout << "\nDecrypted Text : " << M << endl << endl;
+    cout << "Decrypted Secret Key : " << k << endl << endl;
 }
+
+/*
+Enter server's IP address: 127.0.0.1
+Enter port : 8888
+
+Client is connected to Server.
+
+Enter two prime numbers (>100) : 101 181
+
+Public key , PUc = {7477, 18281}
+Private key, PRc = {14413, 18281}
+Public key received from server, PUs = {4551, 13231}
+
+Sent client's Public key to server.
+
+Received encrypted (N1||ID) from server : 9531
+Decrypted Server's ID,   IDs = 29
+Decrypted Server's nonce, N1 = 50
+
+Nonce generated, N2 = 23
+Sent encrypted (N1||N2) to server : 7905
+
+Received encrypted (N2) from server : 15328
+Decrypted Client's Nonce, N2 = 23
+-----Authenticated Server-----
+
+Received cipher from Server : 15001
+Decrypted Secret Key : 454
+*/
--- _12. RSA (Authentication)/server.cpp
+++ _12. RSA (Authentication)/server.cpp
@@ -2,12 +2,12 @@
 # include <arpa/inet.h> 
 using namespace std;
 
-int p, q, n, phi; // global variables
+int p, q, e, d, n, phi; // global variables
 int PUs[2], PRs[2];
 int PUc[2];
 int sock;
 
-int createServer(int port)
+void createServer(int port)
 {
     int sersock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {AF_INET, htons(port), INADDR_ANY};
@@ -27,11 +27,10 @@ int gcd(int a, int b)
 
 void genKey()
 {
-    cout << "\nEnter two prime numbers : "; cin >> p >> q;
-    int n = p * q ;
-    int phi = (p-1) * (q-1);
+    cout << "\nEnter two prime numbers (>100): "; cin >> p >> q;
+    n = p * q ;
+    phi = (p-1) * (q-1);
 
-    int e, d;
     srand(time(NULL));
     do{ e = rand()%(phi-2)+2; } while(gcd(e,phi) != 1);
     for(d=1; d<phi; d++)
@@ -39,19 +38,18 @@ void genKey()
         if((d*e)%phi == 1) break;
     }
     
-    PUs[2] = {e, n}; // public key
-    PRs[2] = {d, n}; // private key
+    PUs[0] = e; PUs[1] = n; // public key
+    PRs[0] = d; PRs[1] = n; // private key
     cout << "\nPublic key , PUs = {" << e << ", " << n << "}" << endl;
     cout <<   "Private key, PRs = {" << d << ", " << n << "}" << endl;
 }
 
-void sharekey()
+void shareKey()
 {
     send(sock, &PUs, sizeof(PUs), 0); // send Server's public key to client
     recv(sock, &PUc, sizeof(PUc), 0); // receive public key from client
-    
+    cout << "Sent Server's Public key to client." << endl;
     cout << "\nPublic key received from client : {" << PUc[0] << ", " << PUc[1] << "}" << endl;
-    cout << "\nSent Server's Public key to client." << endl;
 }
 
 // C = M^e mod n
@@ -76,46 +74,68 @@ int main()
     srand(time(NULL));
 
     createServer(port);
-    genkey();
+    genKey();
     shareKey(); // share public keys
 
-    int ID;  cout<<"\nEnter an ID number : "; cin>>ID;
+    int ID;  cout<<"\nEnter Server's ID number (<100): "; cin>>ID;
     int N1 = rand()%100; // nonce
-    cout << "Nonce, N1 = " << N1;
+    cout << "Nonce generated, N1 = " << N1 << endl;
     
     // step-1: send Enc(PUc, [N1||ID]) to client
     int msg = N1*100 + ID; // append ID to nonce
     int cipher = encrypt(msg, PUc);
     send(sock, &cipher, sizeof(cipher), 0);
-    cout << "\nSent encrypted nonce and ID to client." << endl;
+    cout << "Sent encrypted (N1||ID) to client : " << cipher << endl;
 
     // step-2: recv cipher from client and Dec(PRs, (N1||N2))
     recv(sock, &cipher, sizeof(cipher), 0);
-    cout << "Received encrypted (N1||N2) from client : " << cipher;
+    cout << "\nReceived encrypted (N1||N2) from client : " << cipher << endl;
     msg = decrypt(cipher, PRs);
     int N1c = msg/100; // N1 received from client
     int N2 = msg%100;
-    cout << "\nDecrypted Server's Nonce, N1 = " << N1c;
-    cout << "\nDecrypted Client's Nonce, N2 = " << N2;
-    if(N1 != N1c) {cout << "\nNonce didn't match!"; exit(-1);}
+    cout << "Decrypted Server's Nonce, N1 = " << N1c << endl;
+    cout << "Decrypted Client's Nonce, N2 = " << N2 << endl;
+    if(N1 != N1c) {cout << "\nNonce didn't match!\n"; exit(-1);}
+    else {cout << "-----Authenticated client-----" << endl;}
 
     // step-3: send En(PUc, N2) to client
     cipher = encrypt(N2, PUc);
     send(sock, &cipher, sizeof(cipher), 0);
-    cout << "Sent encrypted N2 to client.";
+    cout << "\nSent encrypted (N2) to client : " << cipher << endl;
 
-    // step-4: send M = En(PUc, En(PRs, k))
+    // step-4: send C = En(PUc, En(PRs, k))
     int k; // secret key
-    cout << "Enter secret key (integer) to send : "; cin >> k;
+    cout << "\nEnter secret key (integer) to send : "; cin >> k;
     cipher = encrypt(encrypt(k,PRs), PUc);
     send(sock, &cipher, sizeof(cipher), 0);
-    cout << "Sent secret key to client."
-
-    int M; // plaintext message
-    cout << "\nEnter message(M<" << PU[1] << ") to encrypt : "; cin >> M;
-
-    int C = encrypt(M, PU);
-    cout << "\nEncrypted Text : " << C << endl;
-    send(sock, &C, sizeof(C), 0); // send ciphertext to client
-    cout << "\nSent ciphertext to client." << endl << endl;
+    cout << "Sent encrypted secret key to client : " << cipher << endl << endl;
 }
+
+/*
+Enter port : 8888
+
+Server Online. Waiting for client....
+Connection Established.
+
+Enter two prime numbers (>100): 101 131
+
+Public key , PUs = {4551, 13231}
+Private key, PRs = {1951, 13231}
+Sent Server's Public key to client.
+
+Public key received from client : {7477, 18281}
+
+Enter Server's ID number (<100): 29
+Nonce generated, N1 = 50
+Sent encrypted (N1||ID) to client : 9531
+
+Received encrypted (N1||N2) from client : 7905
+Decrypted Server's Nonce, N1 = 50
+Decrypted Client's Nonce, N2 = 23
+----Authenticated client----
+
+Sent encrypted (N2) to client : 15328
+
+Enter secret key (integer) to send : 454 
+Sent encrypted secret key to client : 15001
+*/
